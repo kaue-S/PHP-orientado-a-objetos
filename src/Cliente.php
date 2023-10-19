9ec4049677a3448f93c7d3903d072eb03761b505
Kauê Evangelista da Silva Aguiar <140726931+kaue-S@users.noreply.github.com>
2023-10-19 00:01:48 -0300

    Revert "Exemplo02 atribuição e exibição de valores"

--- src/Cliente.php
+++ src/Cliente.php
@@ -5,14 +5,11 @@
         public string $nome;
         public string $email;
         public string $senha;
-        public array $telefones;
+        public array $telefone;
 
         //metodos (ou comportamento)
         public function exibirDados():void{
-            echo "<section>";
-            echo "<h2> $this->nome </h2>";
-            echo "<h2> $this->email </h2>";
-            echo "</section>";
+
         }
     }
 ?>
